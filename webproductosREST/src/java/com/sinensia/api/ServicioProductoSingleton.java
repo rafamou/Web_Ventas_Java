671c95d5d888a22c43da458903795fb7af05e846
rafamou <ragab.fayez2017@gmail.com>
2019-05-30 12:11:02 +0200

    prueba en el fichero LICENSE

---
+++ webproductosREST/src/java/com/sinensia/api/ServicioProductoSingleton.java
@@ -0,0 +1,36 @@
+/* Clase patrón Singleton, sólo puede haber una
+ * instancia (objeto) en toda la aplicación
+ * de este tipo de clase
+ */
+package com.sinensia.api;
+
+import java.util.ArrayList;
+
+public class ServicioProductoSingleton {    
+    
+    private ArrayList<Producto> listaProductos;
+    
+    public void insertar(Producto p) {
+        listaProductos.add(p);
+    }
+    public Producto modificar(Producto p) {
+        p.setNombre(p.getNombre() + " - Modificado");
+        p.setPrecio(p.getPrecio()+ " - Modificado");
+        return p;
+    }
+    public Producto[] obtenerTodos() {
+        return (Producto[]) listaProductos.toArray();
+    }
+ 
+    private static ServicioProductoSingleton instancia = null;    
+
+    private ServicioProductoSingleton() {
+       this.listaProductos = new ArrayList<>();
+    }
+    public static ServicioProductoSingleton getInstancia() {
+        if (instancia == null )
+            instancia = new ServicioProductoSingleton();
+        return instancia;
+    }
+    
+}
